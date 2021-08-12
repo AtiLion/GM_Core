b69adbf0b06040663485433fcaac8ed771748229
AtiLion <AtiLion@users.noreply.github.com>
2021-08-12 13:32:47 +0200

    Working on config merging with JSON

--- Core/PluginLoader.cs
+++ Core/PluginLoader.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json.Linq;
+
 using BepInEx;
 using BepInEx.Harmony;
 using HarmonyLib;
@@ -17,6 +19,8 @@ namespace GM_Core.Core
     internal class PluginLoader : MonoBehaviour
     {
         private Harmony _harmonyInstance;
+
+        private static Dictionary<string, string> _virtualToRealConfig = new Dictionary<string, string>(); // Bad life decisions up ahead
 
         private static MethodInfo _methodDeserializeJson = null;
         private static HarmonyMethod _harmonyDeserializeJson = null;
@@ -41,14 +45,31 @@ namespace GM_Core.Core
             }
         }
 
+        // Called when the JsonRepository in the game finishes deserialization of the file
+        // Then we apply our new JSON values to the old ones???
         private static void JSON_Deserialize(object __instance)
         {
             MethodInfo miJsonFile = __instance.GetType().GetMethod("JsonFile", BindingFlags.Instance | BindingFlags.NonPublic);
             if (miJsonFile == null) return;
 
-            GMCore.Logging.LogInfo("Detected JSON load hit! " + (string)miJsonFile.Invoke(__instance, new object[0]));
+            string virtualPath = (string)miJsonFile.Invoke(__instance, new object[0]);
+            GMCore.Logging.LogInfo("Detected JSON load hit! " + virtualPath);
+            if (!_virtualToRealConfig.ContainsKey(virtualPath)) return;
+
+            string customJson = File.ReadAllText(_virtualToRealConfig[virtualPath]);
+            if (string.IsNullOrEmpty(customJson)) return;
+
+            try
+            {
+                object customObject = JsonUtility.FromJson(customJson, __instance.GetType().BaseType.GetGenericArguments()[1]);
+
+                GMCore.Logging.LogInfo("Successfully loaded custom JSON object for " + virtualPath);
+            }
+            catch(Exception e) { GMCore.Logging.LogError(e); }
         }
 
+        // Loads the JSON modifications of each plugin with a virtual and physical path
+        // Once loaded it stores it into a dictionary _virtualToRealConfig for easier lookup during actual JSON deserialization
         private void LoadJSONPlugin(string path, string virtualPath)
         {
             // Handle directory loading
@@ -65,6 +86,7 @@ namespace GM_Core.Core
             // Handle file loading
             if (!virtualPath.EndsWith(".json")) return;
             GMCore.Logging.LogInfo("Found JSON plugin file: " + virtualPath);
+            _virtualToRealConfig.Add(virtualPath, path);
         }
     }
 }
